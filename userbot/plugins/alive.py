d9917085ffcba5ec4c152fa2c39bc2ca017fec74
TeamAtul <68645655+TeamAtul@users.noreply.github.com>
2020-08-11 09:13:20 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,19 +6,19 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22535f8051a58af113586.jpg"
-pm_caption = "`FRIDAY IS:` **ONLINE**\n\n"
+PM_IMG = "https://telegra.ph/file/fb10fea3d152e1524ef93.png"
+pm_caption = "`E.D.I.T.H. IS:` **ONLINE**\n\n"
 pm_caption += "**SYSTEM STATUS**\n"
 pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
 pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**Friday OS** : `3.14`\n"
-pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
+pm_caption += "**Current Branch** : `piro`\n"
+pm_caption += "**Edith OS** : `3.14`\n"
+pm_caption += "**Current Sat** : `TeamAtulSat-2.25`\n"
+pm_caption += f"**My Master** : {DEFAULTUSER} \n"
 pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "**License** : [MIT Licence](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "Copyright : By [StarkGang@Github](GitHub.com/StarkGang)\n"
-pm_caption += " [Deploy FridayUserbot](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "Copyright : By [TeamAtul@Github](GitHub.com/TeamAtul)\n"
+pm_caption += " [Deploy EdithUserbot](https://telegra.ph/FRIDAY-06-15)"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
